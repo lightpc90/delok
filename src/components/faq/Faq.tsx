c09810deab6405f61d6a4d69e7fe593cd2a54a53
lightpc <afoageektech@gmail.com>
2024-12-03 18:06:58 +0100

    "Updated Faq and EachFaq components with changes to CSS classes and layout"

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -4,7 +4,7 @@ import { faqs } from '@/data/constant'
 
 const Faq = () => {
   return (
-    <div className='bg-neutral w-[90%] max-h-[800px] rounded-lg shadow-lg mb-10 flex flex-col items-center p-5'>
+    <div className='bg-neutral w-[90%] max-w-[600px] max-h-[800px] md:max-h-[1200px] rounded-lg shadow-lg mb-10 flex flex-col items-center p-5'>
       <h2 className='text-xl text-secondary font-bold mb-5'>Frequently Asked Questions</h2>
       <section className='flex flex-col gap-5 w-full items-center max-h-[600px] overflow-y-auto'>
         {faqs.map((faq, i) => (
--- src/components/faq/faqLayout/EachFaq.tsx
+++ src/components/faq/faqLayout/EachFaq.tsx
@@ -4,21 +4,24 @@ import React, { useState } from 'react'
 import { FiArrowRight } from "react-icons/fi";
 import { BsArrowDownCircleFill } from "react-icons/bs";
 
-const EachFaq = ({ faq, i }: { faq: { question: string, answer: string }, i: number}) => {
+const EachFaq = ({ faq, i }: { faq: { question: string, answer: string }, i: number }) => {
     const [isOpen, setIsOpen] = useState(false);
-  return (
-      <div key={i} className='bg-white w-[95%] rounded-lg shadow-md text-slate-950 p-4'>
-          <div onClick={() => setIsOpen(!isOpen)} className='flex items-center gap-2 justify-between pb-4'>
-              <h3 className='font-bold'>{faq.question}</h3>
-              {isOpen ? (
-                  <BsArrowDownCircleFill size={25} />
-              ) : (
-                  <FiArrowRight size={25} />
-              )}
-          </div>
-          {isOpen && <p>{faq.answer}</p>}
-      </div>
-  )
+    return (
+        <div key={i} className=' bg-white w-[95%] rounded-lg shadow-md text-slate-950 p-4'>
+            <div onClick={() => setIsOpen(!isOpen)} className='flex items-center gap-2 justify-between pb-4 cursor-pointer hover:text-primary'>
+                <h3 className='font-bold'>{faq.question}</h3>
+                <div className='flex-shrink-0 flex-grow-0'>
+
+                    {isOpen ? (
+                        <BsArrowDownCircleFill size={25} />
+                    ) : (
+                        <FiArrowRight size={25} />
+                    )}
+                </div>
+            </div>
+            {isOpen && <>{faq.answer}</>}
+        </div>
+    )
 }
 
 export default EachFaq
