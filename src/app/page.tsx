b754612411eddaddbefd506099f459cf42c6d8d1
lightpc <afoageektech@gmail.com>
2024-12-03 14:19:44 +0100

    "Updated PositionedImage component to add 'object-fill' class to Image element"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Faq from "@/components/faq/Faq";
 import Header from "@/components/header/Header";
 import Navbar from "@/components/navbar/Navbar";
 import Products from "@/components/products/Products";
@@ -13,6 +14,7 @@ export default function Home() {
         <section className="flex flex-col w-full gap-8 row-start-3 items-center">
         <ServiceCards />
         <Products />
+        <Faq />
         </section>
       </main>
       <footer className="flex flex-col gap-8 row-start-3 items-center sm:items-start">
