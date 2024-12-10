acd9cbf5678a82ab8c8b7f6706ec71f6ac5fb168
lightpc <afoageektech@gmail.com>
2024-12-11 00:31:33 +0100

    `Update ServiceCards component: change link URLs to '/sell-a-car' and '/rent-a-car'`

--- src/components/servicesCards/ServiceCards.tsx
+++ src/components/servicesCards/ServiceCards.tsx
@@ -22,7 +22,7 @@ const ServiceCards = () => {
         </div>
         <h1 className={`${bebas.className} text-3xl`}>Sell Your Car Fast</h1>
         <p className='w-10/12'>Turn your car into cash effortlessly-- fast, secure, and hassle-free!</p>
-        <Link className='font-bold inline-block w-fit border-2 border-white hover:bg-primaryHover p-2 rounded-md' href={'/'}>
+        <Link className='font-bold inline-block w-fit border-2 border-white hover:bg-primaryHover p-2 rounded-md' href={'/sell-a-car'}>
           Sell Now...</Link>
       </div>
       <div className={`w-[350px] h-[260px] my-[30px] flex flex-col gap-4 p-4 bg-primary rounded-lg shadow-md relative`}>
@@ -34,7 +34,7 @@ const ServiceCards = () => {
         </div>
         <h1 className={`${bebas.className} text-3xl`}>Rent a Car Easily</h1>
         <p className='w-10/12'>Get on the road with ease-- flexible rentals, affordable rates, and zero hassles</p>
-        <Link className='font-bold inline-block w-fit border-2 border-white hover:bg-primaryHover p-2 rounded-md' href={'/'}>
+        <Link className='font-bold inline-block w-fit border-2 border-white hover:bg-primaryHover p-2 rounded-md' href={'/rent-a-car'}>
           Rent Now...</Link>
       </div>
     </div>
